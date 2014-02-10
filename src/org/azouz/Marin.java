0ba3bd1f28bae225c803e83252ec58be9be6ffed
11005455 <11005455@F207-10>
2014-02-10 14:55:31 +0100

    modifier class Marin

---
+++ src/org/azouz/Marin.java
@@ -0,0 +1,16 @@
+package org.azouz;
+
+public class Marin {
+
+	public String nom;
+	public String prenom;
+	public Marin (String nom, String prenom){
+		
+		this.nom=nom;
+		this.prenom=prenom;
+	}
+	
+	
+	
+	
+}
